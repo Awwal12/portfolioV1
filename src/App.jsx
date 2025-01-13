03ed0060b77ca018b494051c0e9befa6d4ca750c
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-13 21:15:57 +0100

    added hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar"
+import Hero from "./components/Hero"
 function App() {
   
 
   return (
     <>
-    <Navbar></Navbar>
+      <Navbar></Navbar>
+      <Hero></Hero>
     </>
-  )
+  );
 }
 
 export default App
