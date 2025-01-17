cc93908c5f124d47396e145ef98f5333f2f01e77
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-17 15:00:20 +0100

    Project section and write up added

---
+++ src/components/projects.jsx
@@ -0,0 +1,24 @@
+export default function Projects() {
+    return (
+      <>
+        <div className="flex mx-auto justify-evenly">
+          <hr class="w-8 h-8  my-8 bg-gray-200 border-0 rounded md:my-12 dark:bg-gray-700"></hr>
+          <hr class="w-8 h-8  my-8 bg-gray-200 border-0 rounded md:my-12 dark:bg-gray-700"></hr>
+          <hr class="w-8 h-8  my-8 bg-gray-200 border-0 rounded md:my-12 dark:bg-gray-700"></hr>
+        </div>
+        <h1 className="text-4xl font-extrabold">Projects</h1>
+        <div className="">
+          <div className="p-8 m-8 text-xl italic font-semibold text-justify text-gray-900 border dark:textborder-white hover:border-blue-500 hover:shadow-lg">
+            In this section, you’ll find a showcase of my journey as a
+            developer, where I’ve combined self-driven designs and
+            collaborations with teams to bring ideas to life. From crafting
+            sleek, accessible interfaces to building robust backend solutions,
+            each project tells a story of growth, learning, and impact. Whether
+            it’s creating tools for local communities or contributing to larger
+            initiatives, I aim to deliver products that are not just functional
+            but meaningful to the people who use them
+          </div>
+        </div>
+      </>
+    );
+}
