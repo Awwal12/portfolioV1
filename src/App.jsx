bf6cdc310a125d4691c4b2987c33d72bb0c204d8
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-21 00:59:51 +0100

    Remove unused ProjectCard component from App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import Navbar from "./components/navbar"
-import Hero from "./components/Hero"
+import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 import Projects from "./components/projects";
-import ProjectCard from "./components/ProjectCard";
 function App() {
-  
-
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
       <Projects></Projects>
-      <ProjectCard></ProjectCard>
     </>
   );
 }
 
-export default App
+export default App;
