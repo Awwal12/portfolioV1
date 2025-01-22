4a245dc72db3bcab537f89b432f319329bb2911a
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-22 12:00:24 +0100

    Adding About section and reorganizing imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/navbar";
-import Hero from "./components/Hero";
 import Projects from "./components/projects";
+import Hero from "./components/Hero";
+import About from "./components/About";
+
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
       <Projects></Projects>
+      <About />
     </>
   );
 }
