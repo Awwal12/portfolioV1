41ed40fca6d34b86b2ae25e6782099b65dfbf1f0
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-24 00:12:18 +0100

    Implement routing with React Router and add Blog component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Projects from "./components/projects";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Contact from "./components/contact";
+import Blog from "./components/Blog";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900">
-      <Navbar />
-      <Hero />
-      <Projects />
-      <About />
-      <Contact />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-white dark:bg-gray-900">
+        <Navbar />
+        <Routes>
+          <Route path="/blog" element={<Blog />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Projects />
+                <About />
+                <Contact />
+              </>
+            }
+          />
+        </Routes>
+        <ScrollToTop />
+      </div>
+    </Router>
   );
 }
 
