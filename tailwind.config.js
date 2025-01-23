d297ec12ff4ac2c8fdcef97a4291da1fe02c4eea
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-23 20:47:23 +0100

    Update Tailwind CSS configuration to include index.html in content paths and enable dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 const flowbite = require("flowbite-react/tailwind");
 
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  darkMode: "class",
   theme: {
     extend: {},
   },
