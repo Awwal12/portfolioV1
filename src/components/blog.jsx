157ac3604b7562cf100e7e4f57120bab1adcf2c7
Mohammed Awwal Mohammed <abbaroze@gmail.com>
2025-01-24 00:12:54 +0100

    Add Blog component with placeholder content and styling

---
+++ src/components/blog.jsx
@@ -0,0 +1,13 @@
+export default function Blog() {
+  return (
+    <section className="py-10 bg-white dark:bg-gray-900">
+      <div className="max-w-[1200px] mx-auto px-8">
+        <h1 className="inline-block p-2 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">
+          Blog
+        </h1>
+        <br />
+        <p className="p-2 text-gray-700 dark:text-gray-300">Coming soon...</p>
+      </div>
+    </section>
+  );
+}
